1b1de45e8bc0860a7ef084045752b967cb7cd749
KevinSDJ <sebadjkevin@gmail.com>
2022-01-10 18:38:57 -0300

    pages number render edit

--- src/components/main/pagesRender.js
+++ src/components/main/pagesRender.js
@@ -28,11 +28,11 @@ export default function PagesRender({ pages, setCurrP, currentPage }) {
     if (pages) {
         return (
         <div className="pg_subcont">
-            <button id="prev" onClick={handleNextOrPrev}><span className="arrow">{'<'}</span></button>
+            <button id="prev" onClick={handleNextOrPrev}>{'<'}</button>
             {currentPage > 1 ? pages.map(e => e < currentPage && <li key={e} id={e} onClick={handleClick}>{e}</li>) : null}
             {pages.map(e => e === currentPage && <li style={{ backgroundColor: "gray",color:"white"}} key={e} id={e} onClick={handleClick}>{e}</li>)}
             {currentPage < pages.length ? pages.map(e => e > currentPage && <li key={e} id={e} onClick={handleClick}>{e}</li>) : null}
-            <button id="next" onClick={handleNextOrPrev} ><span className="arrow">{'>'}</span></button>
+            <button id="next" onClick={handleNextOrPrev} >{'>'}</button>
         </div>
         )
     } else {
