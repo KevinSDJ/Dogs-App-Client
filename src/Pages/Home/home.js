fc35e443eab9cbf71308c80c81751643f3196d13
KevinSDJ <sebadjkevin@gmail.com>
2022-03-03 15:37:29 -0300

    .

--- src/Pages/Home/home.js
+++ src/Pages/Home/home.js
@@ -1,13 +1,12 @@
-import React, { useEffect, useReducer } from 'react';
+import React from 'react';
 import {Outlet} from 'react-router-dom';
 import './home.scss';
-import {useSelector} from 'react-redux'
 
 
 
 
 export default function Home(){
-    let {userData}=useSelector(state=>state)
+
     return(
         <div className="home">
             <div>
