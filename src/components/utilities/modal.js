2047edf3b959b96e1b4d5adf9a09229fb2091f7f
KevinSDJ <sebadjkevin@gmail.com>
2022-03-09 22:51:22 -0300

    .

--- src/components/utilities/modal.js
+++ src/components/utilities/modal.js
@@ -2,14 +2,14 @@ import React ,{useState,useLayoutEffect} from'react';
 import './modal.scss';
 import Success from '../../media/success-svgrepo-com.svg'
 import Warning from '../../media/triangular-warning-signal-svgrepo-com.svg'
-import Error from '../../media/error-svgrepo-com.svg'
+import Err from '../../media/error-svgrepo-com.svg'
 import {clearResponse} from '../../redux/actions/actionsF'
 import {useDispatch,useSelector} from 'react-redux'
 import {useNavigate} from 'react-router-dom'
 
 
 
-let alerts={success:()=>Success,error:()=>Error,warning:()=>Warning}
+let alerts={success:Success,error:Err,warning:Warning}
 export default function Modal({redirectEnd,response}){	
 	const [open,setOpen]=useState(false)
 	let dispatch= useDispatch()
@@ -33,7 +33,7 @@ export default function Modal({redirectEnd,response}){
 		   <div  id="background_modal">
 		       <div id="Modal">
 		         <button id="btnCLoseModal" onClick={handleClick}>x</button>
-		         <div id="Header_Modal">{response&&<img src={response?alerts(response.type):"#"} alt={"message"} /> }<h3 className={response.type+'_modal'}>{response.type}</h3></div>
+		         <div id="Header_Modal">{response&&<img src={response?alerts[`${response.type}`]:"#"} alt={"message"} /> }<h3 className={response.type+'_modal'}>{response.type}</h3></div>
 		         <div id="header_body"><p>{response.msg}</p></div>
 		         <div id="btnModalContent"><button id="btnCLoseModal2" onClick={handleClick}>close</button></div>
 		       </div>
